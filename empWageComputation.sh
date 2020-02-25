4e85667bc89a463c3e353870d0899b5f96c83206
rimadas123 <rdas5969@gmail.com>
2020-02-25 17:30:11 +0530

    Added: To Calculate Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,24 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation"
 
 #check employee is present or absent
 
+#constant values
+empRatePerHr=20
+empHrs=8
+
+#it will give 0 or 1 by using random
 attendance_check=$(( RANDOM%2 ))
-if [ attendance_check -eq 1 ]]
+
+#check the condition if it gives 1 then true
+if [[ $attendance_check -eq 1 ]]
 then
 	echo employee is present
+	salary=$(( $empRatePerHr * $empHrs ))
 else
 	echo employee is absent
+	salary=0
 fi
+
+echo "salary of an employee:" $salary
