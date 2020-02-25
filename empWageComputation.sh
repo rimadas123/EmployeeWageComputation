661c1158449492a46d7284a3f3061a81fcbbc6f1
rimadas123 <rdas5969@gmail.com>
2020-02-25 16:37:23 +0530

    Added : Check Employee is present or absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
 echo "Welcome to Employee Wage Computation"
+
+#check employee is present or absent
+
+attendance_check=$(( RANDOM%2 ))
+if [ attendance_check -eq 1 ]]
+then
+	echo employee is present
+else
+	echo employee is absent
+fi
