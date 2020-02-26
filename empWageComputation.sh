aae50b9fb8cefcbad4ad2ded6af79b96732f2a5e
rimadas123 <rdas5969@gmail.com>
2020-02-26 18:34:30 +0530

    modified : changed constants name to capital letter and did indentation properly

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,12 +2,10 @@
 
 echo "Welcome to Employee Wage Computation"
 
-#check employee is present or absent
-
 #constant values
-empRatePerHr=20
-numberOfWorkingDays=20
-maxHrsInMonth=100
+EMP_RATE_PER_HOUR=20
+NUMBER_OF_WORKING_DAYS=20
+MAX_HRS_IN_MONTH=100
 
 #variables
 totalSalary=0
@@ -15,36 +13,37 @@ totalEmpHr=0
 totalWorkingDays=0
 
 function getWorkingHours() {
-	empCheck=$(( RANDOM%3 + 1 ))
-	case $empCheck in
-		1)
-		empHrs=8
-		;;
-		2)
-		empHrs=4
-		;;
-		3)
-		empHrs=0
-		;;
-	esac
-	echo $empHrs
+   empCheck=$(( RANDOM%3 + 1 ))
+     case $empCheck in
+      1)
+       empHrs=8
+       ;;
+      2)
+       empHrs=4
+       ;;
+      3)
+       empHrs=0
+       ;;
+     esac
+    echo $empHrs
 }
 
 function calculateDailyWage() {
-	local workHours=$1
-	wages=$(( $workHours * $empRatePerHr ))
-	echo $wages
+     local workHours=$1
+     wages=$(( $workHours * $EMP_RATE_PER_HOUR ))
+     echo $wages
 }
 
-while [[ $totalEmpHr -lt $maxHrsInMonth && $totalWorkingDays -lt $numberOfWorkingDays ]]
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUMBER_OF_WORKING_DAYS ]]
 do
-	(( totalWorkingDays++ ))
-	workingHours="$( getWorkingHours )"
-	totalWorkHrs=$(( $totalEmpHr + $workingHours ))
-	empDailyWage["$totalWorkingDays"]="$( calculateDailyWage $workingHours )"
+   (( totalWorkingDays++ ))
+   workingHours="$( getWorkingHours )"
+   totalWorkHrs=$(( $totalEmpHr + $workingHours ))
+   empDailyWage["$totalWorkingDays"]="$( calculateDailyWage $workingHours )"
 done
-	echo $totalWorkHrs
-	#calculate wages per month
-	totalSalary="$( calculateDailyWage $totalWorkHrs )"
-	echo "Daily wage  ${empDailyWage[@]}"
-	echo "All keys  ${!empDailyWage[@]}"
+   echo $totalWorkHrs
+
+   #calculate wages per month
+   totalSalary="$( calculateDailyWage $totalWorkHrs )"
+   echo "Daily wage ${empDailyWage[@]}"
+   echo "All keys  ${!empDailyWage[@]}"
